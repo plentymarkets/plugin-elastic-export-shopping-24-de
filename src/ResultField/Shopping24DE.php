2f195b7b80cb81431832d8905a48952a963cfbd6
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2017-03-03 08:32:58 +0100

    adjusted imageMutator

--- src/ResultField/Shopping24DE.php
+++ src/ResultField/Shopping24DE.php
@@ -129,128 +129,22 @@ class Shopping24DE extends ResultFields
             ],
 
             [
-                $imageMutator,
                 $languageMutator,
                 $skuMutator,
                 $defaultCategoryMutator
             ],
         ];
+
+        if($reference != -1)
+        {
+            $fields[1][] = $imageMutator;
+        }
+
         foreach($itemDescriptionFields as $itemDescriptionField)
         {
             $fields[0][] = $itemDescriptionField;
         }
 
         return $fields;
-//        $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
-//
-//        if($settings->get('variations') == 'mainVariations')
-//        {
-//            $this->setGroupByList(['groupBy.itemIdGetPrimaryVariation']);
-//        }
-//
-//        $itemDescriptionFields = ['urlContent'];
-//        $itemDescriptionFields[] = ($settings->get('nameId')) ? 'name' . $settings->get('nameId') : 'name1';
-//
-//        if($settings->get('descriptionType') == 'itemShortDescription'
-//            || $settings->get('previewTextType') == 'itemShortDescription')
-//        {
-//            $itemDescriptionFields[] = 'shortDescription';
-//        }
-//
-//        if($settings->get('descriptionType') == 'itemDescription'
-//            || $settings->get('descriptionType') == 'itemDescriptionAndTechnicalData'
-//            || $settings->get('previewTextType') == 'itemDescription'
-//            || $settings->get('previewTextType') == 'itemDescriptionAndTechnicalData')
-//        {
-//            $itemDescriptionFields[] = 'description';
-//        }
-//
-//        if($settings->get('descriptionType') == 'technicalData'
-//            || $settings->get('descriptionType') == 'itemDescriptionAndTechnicalData'
-//            || $settings->get('previewTextType') == 'technicalData'
-//            || $settings->get('previewTextType') == 'itemDescriptionAndTechnicalData')
-//        {
-//            $itemDescriptionFields[] = 'technicalData';   done
-//        }
-//
-//        return [
-//            'itemBase'=> [
-//                'id',                 done
-//                'producerId',         done
-//            ],
-//
-//            'itemDescription' => [
-//                'params' => [
-//                    'language' => $settings->get('lang') ? $settings->get('lang') : 'de',
-//                ],
-//                'fields' => $itemDescriptionFields,       done
-//            ],
-//
-//            'variationImageList' => [
-//                'params' => [
-//                    'type' => 'item_variation',
-//                    'referenceMarketplace' => $settings->get('referrerId') ? $settings->get('referrerId') : -1,
-//                ],
-//                'fields' => [
-//                    'type',           done
-//                    'path',           done
-//                    'position',       done
-//                ]
-//            ],
-//
-//            'variationRecommendedRetailPrice' => [
-//                'params' => [
-//                    'referrerId' => $settings->get('referrerId'),
-//                ],
-//                'fields' => [
-//                    'price',          todo grab from idl
-//                ],
-//            ],
-//
-//            'variationBase' => [
-//                'availability',               done
-//                'attributeValueSetId',        done
-//                'model',                      done
-//                'limitOrderByStockSelect',    done
-//                'unitId',                     done
-//                'customNumber',               todo grab from idl
-//                'content',                    done
-//            ],
-//
-//            'variationBarcodeList' => [
-//                'variationId',                not needed
-//                'code',                       done
-//                'barcodeId',                  ?
-//                'barcodeType',                done
-//                'barcodeName',                ?
-//            ],
-//
-//            'variationRetailPrice' => [
-//                'params' => [
-//                    'referrerId' => $settings->get('referrerId'),
-//                ],
-//                'fields' => [
-//                    'price',                  todo grab from idl
-//                ],
-//            ],
-//
-//            'variationStandardCategory' => [
-//                'params' => [
-//                    'plentyId' => $settings->get('plentyId'),
-//                ],
-//                'fields' => [
-//                    'categoryId',             done
-//                    'plentyId',
-//                    'manually',
-//                ],
-//            ],
-//
-//            'itemPropertyList' => [
-//                'itemPropertyId',             todo grab from idl
-//                'propertyId',                 todo grab from idl
-//                'propertyValue',              todo grab from idl
-//                'propertyValueType',          todo grab from idl
-//            ],
-//        ];
     }
 }
